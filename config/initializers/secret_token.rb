e36ccb987177e06007455fb68af2b1b70074cf9e
Zhu Jia Cheng <realalien@gmail.com>
2011-04-21 14:15:22 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodsMonitor::Application.config.secret_token = 'e5231922d125515c8adfa6194f009f8c87b01cb2350883210b7fd7018876b4bf230d5c62c5c04f22bdfc1fea250262c4dd04b3f3f77dd490ba83d4970a805776'
